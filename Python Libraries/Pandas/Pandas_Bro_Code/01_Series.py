893e04904e542b1e4605c9ef680338eed4424add
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-21 02:02:14 +0530

    Series in Pandas

--- Python Libraries/Pandas/Pandas_Bro_Code/01_Series.py
+++ Python Libraries/Pandas/Pandas_Bro_Code/01_Series.py
@@ -41,3 +41,37 @@ print(series2.iloc[2])
 
 series2.iloc[2] = 103
 print(series2, end = "\n\n")
+
+#4-- Filter by value
+
+data3 = [101, 102, 103, 201, 202]
+
+# series3 = pd.Series(data3, index = ['a', 'b', 'c']) #This gives an error for data3
+
+series3 = pd.Series(data3, index = ['a', 'b', 'c', 'd', 'e'])
+
+#Printing values > 200 
+print(series3[series3 > 200], end = "\n\n")
+
+#Printing 102 with index and metadata
+print(series3[series3 == 102], end = "\n\n")
+
+#5-- Using dictionary as data instead of list..abs
+
+data4 = {1 : "Pikachu", 2 : "Bulbasaur", 3 : "CharLizard", 4 : "PsyDuck"}
+
+series4 = pd.Series(data4)
+
+print(series4, end = "\n\n")
+
+#NOTE::(IMPORTANT)
+
+# data5 = {1 : "Pikachu", 2 : "Bulbasaur", 3 : "CharLizard", 4 : "PsyDuck"}
+
+# series5 = pd.Series(data5, index = ["Pokemon1", "Pokemon2"])
+
+# print(series5, end = "\n\n")
+
+##The code above doesnt give output as intended.
+##For dictionaries, The keys provided with dictionary are used as ultimate truth in pandas
+##What we put as index in pd.Seres, is something which python looks for in dict, but since it couldnt find it, it returns NaN

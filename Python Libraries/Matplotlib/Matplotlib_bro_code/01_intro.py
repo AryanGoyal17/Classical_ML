65dcc96e1e46b04f9f89e029a047b8f72fe99b54
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-03-18 03:57:42 +0530

    cleared the doubt regarding continous use of plt.plot

--- Python Libraries/Matplotlib/Matplotlib_bro_code/01_intro.py
+++ Python Libraries/Matplotlib/Matplotlib_bro_code/01_intro.py
@@ -18,16 +18,11 @@ import numpy as np
 x = np.array([2023, 2024, 2025, 2026]) #years
 y = np.array([10, 16, 12, 13]) #class-sizes
 
-#2--DONT put 2 plt.plots in same line.. as they dont give intended results
-
-##plt.plot(x, y) #first argument -- points in x axis, second argument -- points in y axis. 
-##plt.plot(y)
-
-#3--
+#2--
 plt.plot(x, y) #first argument -- points in x axis, second argument -- points in y axis.
 plt.show() # to show the plot
 
-#4--
+#3--
 ##If only 1 argument is given, it is taken as values of y axis, and x axis -- takes as 0,1,2,3...
 plt.plot(x)
 plt.show()
